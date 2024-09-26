8102c5e7677be2ed729e599f48b271cc76843ff1
nomo9133 <nomo9133@users.noreply.github.com>
2024-09-26 16:51:22 +0800

    錯誤修正
    1.變數讀取
    2.年齡驗證使用POST
    3.增加未輸入帳號密碼錯誤處理
    4.腳本結束修正

--- check_in/ptt/pttLogin.js
+++ check_in/ptt/pttLogin.js
@@ -1,19 +1,19 @@
-// Boxjs Ptt Login Bot
+// Ptt 自動登入 Bot
 
 // 取得 Boxjs 中儲存的帳號密碼
-let username = $persistentStore.read("ptt_username") || "";
-let password = $persistentStore.read("ptt_password") || "";
+let username = $persistentStore.read("ptt.username");
+let password = $persistentStore.read("ptt.password");
 
 if (!username || !password) {
-  console.log("請在 Boxjs 中設置 Ptt 帳號和密碼");
-  $notification.post("Ptt Login Bot", "錯誤", "未設置帳號密碼");
+  console.log("帳號或密碼未設定，請在 Boxjs 中設置");
+  $notification.post("Ptt Login Bot", "錯誤", "請在 Boxjs 中設置帳號密碼");
   $done();
 }
 
 // Ptt 登入的 URL
 const loginUrl = "https://www.ptt.cc/ask/over18";
 
-// 登入流程
+// 進行登入
 function login() {
   $httpClient.post({
     url: loginUrl,
@@ -25,14 +25,13 @@ function login() {
     if (error) {
       console.log("登入失敗：" + error);
       $notification.post("Ptt Login Bot", "登入失敗", error);
-      $done();
     } else {
       console.log("登入成功");
       $notification.post("Ptt Login Bot", "登入成功", "已成功登入 Ptt");
-      $done();
     }
+    $done();
   });
 }
 
-// 如果帳號和密碼存在，進行登入
+// 呼叫登入函數
 login();
